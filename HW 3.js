b3653abb507ed3c67d9c2f0dfb14d3da77caa589
StiveTitov <123_dem_321@mail.ru>
2023-09-03 16:14:11 +0300

    Первое задание

---
+++ HW 3.js
@@ -0,0 +1,9 @@
+// Задание 1
+let password = 'admin';
+let answer = prompt('Введите пароль');
+if (answer === password) {
+    console.log('Пароль введен верно');
+}
+else {
+    console.log('Пароль введен неправильно');
+}

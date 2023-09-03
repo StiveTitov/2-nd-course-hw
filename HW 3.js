207121dbb84f2ec0a9f7d98e07f8cf527027a376
StiveTitov <123_dem_321@mail.ru>
2023-09-03 17:21:51 +0300

    Задание 5

--- HW 3.js
+++ HW 3.js
@@ -25,3 +25,48 @@ let b = '3';
 a = a * 1;
 b = b * 1;
 alert(a + b);
+
+// Задание 5
+let monthNumber = Number(prompt('Введите номер месяца'));
+switch (monthNumber) {
+    case 1:
+        console.log('Зима');
+        break;
+    case 2:
+        console.log('Зима');
+        break;
+    case 3:
+        console.log('Весна');
+        break;
+    case 4:
+        console.log('Весна');
+        break;
+    case 5:
+        console.log('Весна');
+        break;
+    case 6:
+        console.log('Лето');
+        break;
+    case 7:
+        console.log('Лето');
+        break;
+    case 8:
+        console.log('Лето');
+        break;
+    case 9:
+        console.log('Осень');
+        break;
+    case 10:
+        console.log('Осень');
+        break;
+    case 11:
+        console.log('Осень');
+        break;
+    case 12:
+        console.log('Зима');
+        break;
+    default:
+        console.log('Такого месяца не существует!');
+
+        break;
+}

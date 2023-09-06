da2701423ef6a17b3d72c7385b5bf5d176a5c12e
StiveTitov <123_dem_321@mail.ru>
2023-09-07 01:18:46 +0300

    переделал задания: 2, 3.1, 3.2

--- HW 5.js
+++ HW 5.js
@@ -14,21 +14,26 @@ printMessage();
 // Задание 2
 console.log('Задание 2');
 const getNumber = (number) => {
-    number = Number(prompt('Введите число'));
     if (number % 2 === 0) {
         console.log(`Число ${number} -четное`);
         return
     }
     console.log(`Число ${number} -нечетное`);
 }
-getNumber();
+getNumber(Number(prompt('Введите число')));
 
 // Задание 3.1
-console.log('Задание 3');
+console.log('Задание 3.1');
 const getUserNumber = (userNumber) => {
-    userNumber = Number(prompt('Введите число'));
-    userNumber = userNumber ** 2;
-    console.log(`Ответ: ${userNumber}`);
+    console.log(`Ответ: ${userNumber ** 2}`);
 }
-getUserNumber();
+getUserNumber(Number(prompt('Введите число')));
+
+// Задание 3.2
+console.log('Задание 3.2');
+const getSecondNumber = (userSecondNumbe) => {
+    alert(userSecondNumbe ** 2);
+}
+getSecondNumber(Number(prompt('Введите число')))
+
 

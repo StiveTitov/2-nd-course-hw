eeb4372ddffe056475a6a6ad6c18f4028462624a
StiveTitov <123_dem_321@mail.ru>
2023-09-10 02:01:06 +0300

    выполнил задание 4

--- HW 6.js
+++ HW 6.js
@@ -15,3 +15,17 @@ console.log(arry.indexOf(4));
 console.log('Задание 3');
 arry = [1, 3, 5, 10, 20];
 console.log(arry.join(' '));
+
+// Задание 4
+console.log('Задание 4');
+let arr = [];
+
+for (i = 0; i < 3; i++) {
+    arr[i] = []; // создаем подмассив
+
+    for (let j = 0; j < 3; j++) {
+        arr[i].push(1); // заполняем подмассив числом 1
+    }
+}
+
+console.log(arr);

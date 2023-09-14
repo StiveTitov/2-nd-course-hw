b15228f27cbdeaa91d52252335f8fe144f31f1c7
StiveTitov <123_dem_321@mail.ru>
2023-09-14 21:57:23 +0300

    Выполнил задание 2

--- HW 7.js
+++ HW 7.js
@@ -3,3 +3,21 @@ console.log('Задание 1');
 
 let str = 'js';
 console.log(str.toUpperCase());
+
+// Задание 2
+
+const searchStart = (products, search) => {
+    products.forEach((product) => {
+
+        if (product.toLowerCase().includes(search.toLowerCase())) {
+            let result = [];
+            result.push(product);
+            console.log(result);
+        }
+        return
+    });
+}
+
+searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'); // ['кошка', 'комар']
+searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'); // ['груша']
+searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'); // []

5cf5933e7da93fd1eac2c475ea4d6616f0d56407
StiveTitov <123_dem_321@mail.ru>
2023-09-15 22:44:53 +0300

    сделал задание 6 и 7

--- HW 7.js
+++ HW 7.js
@@ -51,3 +51,33 @@ getRandomIntInclusive(1, 10);
 // Задание 6
 console.log('Задание 6');
 
+const getRandomArrNumbers = (max) => {
+    max = Math.floor(max);
+    let arry = [];
+    let lengthArry = Math.floor(max / 2);
+    for (i = 0; i < lengthArry; i++) {
+        arry.push(Math.floor(Math.random() * (max + 1)));
+    }
+    console.log(arry);
+    return;
+}
+getRandomArrNumbers(7); // [6, 2, 7] - массив заполнен случайными числами
+// от 0 до 7, длина массива 7 / 2 = 3.5, округляем до ближайшего меньшего
+// числа, получаем 3
+getRandomArrNumbers(12); // [9, 11, 10, 9, 3, 0] - массив заполнен случайными числами
+// от 0 до 12, длина массива 12 / 2 = 6
+getRandomArrNumbers(100);
+getRandomArrNumbers(89);
+
+
+// Задание 7
+console.log('Задание 7');
+
+const getRandomInclusive = (a, b) => {
+    a = Math.ceil(a);
+    b = Math.floor(b);
+    console.log(Math.floor(Math.random() * (b - a + 1)) + a);
+    return;
+}
+getRandomInclusive(3, 7);
+

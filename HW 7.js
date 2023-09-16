f40a1169ec51e5c6fd9be8cdef873750f7676cda
StiveTitov <123_dem_321@mail.ru>
2023-09-16 20:11:01 +0300

    Задание 10- готово

--- HW 7.js
+++ HW 7.js
@@ -94,4 +94,34 @@ let currentDate = new Date();
 currentDate.setDate(73);
 console.log(currentDate);
 
+// Задание 10
+console.log('Задание 10');
 
+const funcDate = (newDate) => {
+    let days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    // создадим массив, где будем хранить названия месяцев на русском
+    let months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+        "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+
+    let hour = newDate.getHours(); // получаем час из нашей даты
+    let minute = newDate.getMinutes(); // получаем минуты
+    let second = newDate.getSeconds(); // получаем секунды
+
+    if (hour < 10) { // если будет меньше 10,
+        hour = "0" + hour; // то перед ними поставим 0
+    }
+    if (minute < 10) { // если минут будет меньше 10,
+        minute = "0" + minute; // то перед ними поставим 0
+    }
+    if (second < 10) { // если секунд будет меньше 10,
+        second = "0" + second; // то перед ними поставим 0
+    }
+    let fullDate = "Дата: " + newDate.getDate() + // getDate возвращает число
+        " " + months[newDate.getMonth()] + // getMonth возвращает номер месяца, который мы можем использовать в качестве индекса для массива months
+        " " + newDate.getFullYear() + // getFullYear возвращает год
+        ", " + days[newDate.getDay()] + // getDay возвращает номер дня недели, который мы используем в качестве индекса для массива days
+        "\n" + "Время: " + hour + ":" + minute + ":" + second;
+
+    console.log(fullDate);
+}
+funcDate(new Date());

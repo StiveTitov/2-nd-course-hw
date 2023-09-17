e5a28ddb594856a89e34462e2b325c613bdde159
StiveTitov <123_dem_321@mail.ru>
2023-09-18 00:02:05 +0300

    5 задание - не знаю как делать

--- HW 8.js
+++ HW 8.js
@@ -168,3 +168,29 @@ function delayForSecond(callback) {
 delayForSecond(function () {
     console.log('Привет, Глеб!');
 })
+
+// Задание 5
+console.log('Задание 5');
+
+//Правильный порядок:
+
+//Прошла одна секунда
+//Привет, Глеб!
+
+// Функция delayForSecond через 1 секунду пишет в консоль «Прошла одна секунда», 
+// а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if (cb) { cb(); }
+    }, 1000)
+}
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
+// Код выше менять нельзя
+
+// Нужно изменить код ниже:
+delayForSecond(0)
+delayForSecond(sayHi('Глеб'))

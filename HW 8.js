f43bad08c81b938836cd091e4815c20f51646f27
StiveTitov <123_dem_321@mail.ru>
2023-09-17 21:33:16 +0300

    2 задание- не знаю как делать

--- HW 8.js
+++ HW 8.js
@@ -15,24 +15,67 @@ callbackWithArrayLength([1, 1, 1, 1, 1], (l) => {
     console.log(l);
 });
 
-// Задание 2
+// Задача 2
+console.log('------------------');
 function log(arrItem) {
     console.log("Элемент массива:", arrItem);
 }
 
 function each(arr, cb) {
     for (let i = 0; i < arr.length; i++) {
-        cb(i);
+        cb(arr[i]);
 
     }
 }
 
 each(["Глеб", "Олег", "Татьяна", "Платон"], log);
 
+console.log('------------------');
+
+function add5(number) {
+    return number + 5;
+}
+
+function add10(number) {
+    return number + 10;
+}
+
+console.log(add5(3)); // => 8
+console.log(add10(3)); // => 13
+
+console.log('------------------');
+
+function square(number) {
+    return number * number;
+}
+
+function squareRoot(number) {
+    return Math.sqrt(number);
+}
+
+function map(arr, ruleFunction) {
+    const output = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        output.push(ruleFunction(arr[i]));
+    }
+
+    return output;
+}
+
+console.log(map([1, 4, 9, 16], square)); // => [ 1, 16, 81, 256 ]
+console.log(map([1, 4, 9, 16], squareRoot)); // => [ 1, 2, 3, 4 ]
+console.log(map([1, 1, 2, 3, 5, 8], (number) => number * 2)); // => [2, 2, 4, 6, 10, 16]
+console.log(map([1, 1, 2, 3, 5, 8], (number) => number + 10)); // => [11, 11, 12, 13, 15, 18]
+console.log('-----------------------------------');
+
+
+
+
 // Задание 1
 console.log('Задание 1');
 
-const people = [
+let people = [
     { name: 'Глеб', age: 29 },
     { name: 'Анна', age: 17 },
     { name: 'Олег', age: 7 },
@@ -49,10 +92,46 @@ console.log(people.sort(function (varA, varB) {
     }
     return 0;
 }));
- // код выше должен вывеcти =>
- // [
- //  { name: 'Олег', age: 7 },
- //  { name: 'Анна', age: 17 },
- //  { name: 'Глеб', age: 29 },
- //  { name: 'Оксана', age: 47 }
- // ]
+// код выше должен вывеcти =>
+// [
+//  { name: 'Олег', age: 7 },
+//  { name: 'Анна', age: 17 },
+//  { name: 'Глеб', age: 29 },
+//  { name: 'Оксана', age: 47 }
+// ]
+
+// Задание 2
+console.log('Задание 2');
+
+people = [
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
+];
+
+function isPositive(number) {
+    // писать код тут
+    if (number > 0) {
+
+        return number;
+    }
+}
+function isMale() {
+    // писать код тут
+}
+function filter(arr, ruleFunction) {
+    // писать код тут
+    const output = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        output.push(ruleFunction(arr[i]));
+    }
+
+    return output;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
+
+
+console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
